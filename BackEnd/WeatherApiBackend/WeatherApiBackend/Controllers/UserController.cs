62402c0a190c29b8f804e57527ad1a775a36b1fd
Gorjan <gokikuzmanovski@gmail.com>
2023-11-05 19:39:39 +0100

    Fixed BE
    added Auth, user model , Db and migrations

---
+++ BackEnd/WeatherApiBackend/WeatherApiBackend/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WeatherApiBackend.Context;
+using WeatherApiBackend.Models;
+
+namespace WeatherApiBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        public UserController(AppDbContext appDbContext)
+        {
+            _appDbContext= appDbContext;
+        }
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+
+            var user = await _appDbContext.Users.FirstOrDefaultAsync( x =>
+
+                x.Email  == userObj.Email && x.Password == userObj.Password
+            );
+            if (user == null)
+                return NotFound(new { Message = "User not found! " });
+            return Ok(new
+            {
+                Message = "Login Success! "
+            });
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+
+            await _appDbContext.Users.AddAsync(userObj);
+            await _appDbContext.SaveChangesAsync();
+            return Ok(new
+            {
+                Message = "User Registered Successfully! "
+            });
+        }
+    }
+}
